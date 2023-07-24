20608e813136636a0d9fbfbd2eb2c8a790ea0fd9
BilalLaaroussiElAlami <bilal.laaroussi.el.alami@vub.be>
2023-07-24 12:20:07 +0200

    added bilal_run_benchmark_sh

---
+++ bilal_run_benchmarks.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# set -e makes the script exit if any command fails
+# set -u makes the script exit if any unset variable is used
+# set -o pipefail makes the script exit if any command in a pipeline fails
+set -euo pipefail
+
+for ((i=64; i>=1; i--))
+do
+    echo "> send_message_bilal, $i threads"
+    erl +S $i -noshell -s benchmarkBilal test_send_message_bilal -s init stop > "benchmarks/result-send_message-$i.txt"
+done
+”
